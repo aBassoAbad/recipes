d903db614ae81835472ccaa7ba115cca6c879fcc
Alejandro Basso <alejandro.basso.abad.dam@gmail.com>
2022-04-20 04:02:22 -0400

    Punto 83 curso

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root "pages#home"
+    get "pages/home", to: "pages#home"
+
+    resources :recipes do
+        resources :comments, only: [:create]
+        member do 
+          post 'like'
+        end
+      end
+
+    get "/signup", to: "chefs#new"
+    resources :chefs, except: [:new]
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
+    resources :ingredients, except: [:destroy]
+
+    mount ActionCable.server => "/cable"
+    get "/chat", to: "chatrooms#show"
+
+    resources :messages, only: [:create]
 end
